6b525fdf6957dfb2b2b79a87afd2806caf09c438
Nah-oh <nao.m-lanao@epitech.eu>
2024-03-23 14:31:41 +0400

    [ADD] Map on the application

--- OpenProject/components/HomeScreen.js
+++ OpenProject/components/HomeScreen.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>App map placeholder</Text>
-    </View>
-  );
-}
+    return (
+      <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: -20.964885623394416,
+        longitude: 55.6592007697144,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+    );
+  }
--- OpenProject/components/Settings.js
+++ OpenProject/components/Settings.js
@@ -1,10 +1,35 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableHighlight, Dimensions, StyleSheet } from 'react-native';
 
 export function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
+    <View style={styles.settingsView}>
+            <TouchableHighlight
+                style={styles.circle}
+            >
+                <Text> Mom, look, I am a circle! </Text>
+            </TouchableHighlight>
+            <Text style={
+                {
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }
+            }>About me</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    settingsView: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    circle: {
+        borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+        width: Dimensions.get('window').width * 0.2,
+        height: Dimensions.get('window').width * 0.2,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+});
