82238a25b1606838378cc4996520410a9567c1da
Vincent <vincent.gardebien@epitech.eu>
2024-03-23 15:02:43 +0400

    add message box

--- OpenProject/components/FeedScreen.js
+++ OpenProject/components/FeedScreen.js
@@ -1,10 +1,32 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 
 export function FeedScreen() {
+  const [text, setText] = React.useState('');
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed!</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="Type here..."
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column-reverse',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '80%',
+  },
+});
