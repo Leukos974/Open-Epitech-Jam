172e7ab152a33217677621f43c8fec3754c2e8dd
Nah-oh <nao.m-lanao@epitech.eu>
2024-03-23 13:27:06 +0400

    [ADD] Tab bottom bar navigation

--- OpenProject/App.js
+++ OpenProject/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from './components/HomeScreen';
+import { SettingsScreen } from './components/Settings';
+import { FeedScreen } from './components/FeedScreen';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function Navigator() {
   return (
-    <View style={styles.container}>
-      <Text>hello app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+
+        }}
+      />
+      <Tab.Screen
+      name="Feed"
+      component={FeedScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome name="comment-o" color={color} size={size} />
+        ),
+      }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
+  );
+}
